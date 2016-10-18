9b6a404249a0ae8e20617c897195418dd20feae4
Laurence45 <laurence.lutz@pole-emploi.fr>
2016-10-18 16:42:54 +0200

    Add files via upload

--- HelloLaPromo.java
+++ HelloLaPromo.java
@@ -6,6 +6,7 @@ public class HelloLaPromo {
 	public static void main(String[] args) {
 		List<String> membres = new ArrayList();
 		membres.add("Eric");
+		membres.add("laurence");
 
 		for(String membre : membres) {
 			System.out.println(membre);
