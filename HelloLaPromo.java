ef4fbbe6e6a03fc6c26d90ed00986aea24514658
KodKlod <codingklod@gmail.com>
2016-10-18 16:05:33 +0200

    Update HelloLaPromo.java
    Changement du prénom

--- HelloLaPromo.java
+++ HelloLaPromo.java
@@ -5,7 +5,7 @@ public class HelloLaPromo {
 	
 	public static void main(String[] args) {
 		List<String> membres = new ArrayList();
-		membres.add("Eric");
+		membres.add("Claude");
 
 		for(String membre : membres) {
 			System.out.println(membre);
