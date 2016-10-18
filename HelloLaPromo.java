cfb6363a709882313cb9821ffdc82237fad45c0a
Laurent <laurent.haillot@orange.fr>
2016-10-18 16:28:24 +0200

    Update HelloLaPromo.java

--- HelloLaPromo.java
+++ HelloLaPromo.java
@@ -6,6 +6,7 @@ public class HelloLaPromo {
 	public static void main(String[] args) {
 		List<String> membres = new ArrayList();
 		membres.add("Eric");
+		membres.add("Laurent");
 
 		for(String membre : membres) {
 			System.out.println(membre);
